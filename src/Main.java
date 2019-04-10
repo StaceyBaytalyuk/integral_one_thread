5a04fa7fe7c92922bddbc72f3d3e2d27384a865b
stacey <stacey.baytalyuk@gmail.com>
2019-04-10 21:56:53 +0300

    Delete runtime

--- src/Main.java
+++ src/Main.java
@@ -8,28 +8,9 @@ public class Main {
         double b = 2;
         int n = 10_000_000;
         IntegralCalculator calculator = new IntegralCalculator(a, b, n);
-        printCalculationTime(calculator);
-    }
-
-    private void printCalculationTime(IntegralCalculator calculator) {
-        long start = System.currentTimeMillis();
-        System.out.println(calculator.leftRectangle());
-        double time = (double)(System.currentTimeMillis()-start)/1000;
-        System.out.println("Метод лівих прямокутників: " + time + " секунд\n");
-
-        start = System.currentTimeMillis();
-        System.out.println(calculator.rightRectangle());
-        time = (double)(System.currentTimeMillis()-start)/1000;
-        System.out.println("Метод правих прямокутників: " + time + " секунд\n");
-
-        start = System.currentTimeMillis();
-        System.out.println(calculator.trapezoid());
-        time = (double)(System.currentTimeMillis()-start)/1000;
-        System.out.println("Метод трапецій: " + time + " секунд\n");
-
-        start = System.currentTimeMillis();
-        System.out.println(calculator.simpson());
-        time = (double)(System.currentTimeMillis()-start)/1000;
-        System.out.println("Метод Сімпсона: " + time + " секунд\n");
+        System.out.println("Метод лівих прямокутників: " + calculator.leftRectangle());
+        System.out.println("Метод правих прямокутників: " + calculator.rightRectangle());
+        System.out.println("Метод трапецій: " + calculator.trapezoid());
+        System.out.println("Метод Сімпсона: " + calculator.simpson());
     }
 }
